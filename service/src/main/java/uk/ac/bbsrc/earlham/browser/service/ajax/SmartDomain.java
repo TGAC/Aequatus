81ea1cae526919621f32ccdd94c266468ecce8ab
thankia <anil.thanki@tgac.ac.uk>
2017-05-02 10:50:31 +0100

    adds method to check when search domain is queued

--- service/src/main/java/uk/ac/bbsrc/earlham/browser/service/ajax/SmartDomain.java
+++ service/src/main/java/uk/ac/bbsrc/earlham/browser/service/ajax/SmartDomain.java
@@ -15,6 +15,8 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thankia on 11/04/2017.
@@ -58,40 +60,125 @@ public class SmartDomain {
 
             DataInputStream input = new DataInputStream(connection.getInputStream());
 
-            String str;
-            StringBuilder sb = new StringBuilder();
-            JSONArray domains = new JSONArray();
-            JSONObject domain = new JSONObject();
+            JSONObject response = new JSONObject();
 
-            while (null != (str = input.readLine())) {
-                sb.append(str);
-                if (str.contains("=")) {
-                    if (str.split("=")[0].equals("DOMAIN")) {
-                        domain = new JSONObject();
-                    }
+            String firstLine = input.readLine();
+            if (firstLine.indexOf("RESULT") > 0) {
+                response = parseDomain(input);
+                response.put("status", "finished");
+            } else {
+                String str;
+                StringBuilder sb = new StringBuilder();
+                sb.append(firstLine);
 
-                    domain.put(str.split("=")[0], str.split("=")[1]);
+                while (null != (str = input.readLine())) {
+                    sb.append(str);
+                }
 
-                    if (str.split("=")[0].equals("STATUS")) {
-                        domains.add(domain);
-                    }
+                String pattern = "(job_status\\.pl\\?jobid=)(\\d+.+?)\"";
+                response.put("jobid", "queued");
+                Pattern r = Pattern.compile(pattern);
+                Matcher m = r.matcher(sb.toString());
+                if (m.find()) {
+                    response.put("status", "queued");
+                    response.put("jobid", m.group(2));
+                } else {
+                    response.put("status", "error");
+                    response.put("error", "SMART returned an error page");
+                    response.put("string", firstLine);
                 }
             }
 
+            String str;
+            StringBuilder sb = new StringBuilder();
+
+            while (null != (str = input.readLine())) {
+                sb.append(str);
+            }
+
+            response.put("html", sb.toString());
+
             input.close();
             connection.disconnect();
-
-            String result = null;
-            result = sb.toString();
-
-            JSONObject response = new JSONObject();
-            response.put("domains", domains);
-            response.put("html", result);
 
             return response;
         } catch (Exception e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             return JSONUtils.SimpleJSONError(e.getMessage());
         }
+    }
+
+
+    private JSONObject parseDomain(DataInputStream input) throws IOException {
+        String str;
+        StringBuilder sb = new StringBuilder();
+        JSONObject response = new JSONObject();
+
+        JSONArray domains = new JSONArray();
+        JSONObject domain = new JSONObject();
+
+        while (null != (str = input.readLine())) {
+            sb.append(str);
+            if (str.contains("=")) {
+                if (str.split("=")[0].equals("DOMAIN")) {
+                    domain = new JSONObject();
+                }
+
+                domain.put(str.split("=")[0], str.split("=")[1]);
+
+                if (str.split("=")[0].equals("STATUS")) {
+                    domains.add(domain);
+                }
+            }
+        }
+
+        response.put("domains", domains);
+
+        return response;
+    }
+
+    public JSONObject jobStatus(HttpSession session, JSONObject json) throws IOException {
+        String jobid = json.getString("jobid");
+
+        URL url = new URL("http://smart.embl.de/smart/job_status.pl?jobid=" + jobid);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setInstanceFollowRedirects(false);
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("charset", "utf-8");
+        connection.setUseCaches(false);
+
+        DataInputStream input = new DataInputStream(connection.getInputStream());
+        StringBuffer sb = new StringBuffer();
+
+        String firstLine = input.readLine();
+
+        JSONObject response = new JSONObject();
+
+
+        response.put("firstLine", firstLine);
+
+        if (firstLine.indexOf("HTML") > 0) {
+            response.put("status", "queued");
+        } else {
+            response = parseDomain(input);
+            response.put("status", "finished");
+        }
+
+        String str;
+
+        while (null != (str = input.readLine())) {
+            sb.append(str);
+        }
+
+        response.put("html", sb.toString());
+
+
+        input.close();
+
+        return response;
     }
 }
